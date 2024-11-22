275a829eeaed8c43f0d441c00a0de2bb285cf38a
anouar <anouar.ab95@gmail.com>
2024-11-22 16:46:43 +0100

    CIT-60 genner Test unitaire

--- citronix/src/main/java/com/projet/citronix/repository/ChampRepository.java
+++ citronix/src/main/java/com/projet/citronix/repository/ChampRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ChampRepository extends JpaRepository<Champ, Long> {
+
+    Optional<Champ> findByNom(String nom);
 
     @Query(value = "SELECT c FROM Champ c LEFT JOIN FETCH c.ferme",
            countQuery = "SELECT COUNT(c) FROM Champ c")
